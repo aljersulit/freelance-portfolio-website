abde2e0cdda181281af2a113cef4221fd9db5e13
Aldrin <aljeromesulit@gmail.com>
2025-02-15 11:12:00 +0800

    Add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Hello World!</h1>
+			<Button variant="default">Button</Button>
 		</main>
 	);
 }
+
