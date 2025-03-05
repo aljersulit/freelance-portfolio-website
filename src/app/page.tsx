a97124b3e76ce0145d52fdd7f51fd7349c7191c7
Aldrin <aljeromesulit@gmail.com>
2025-03-05 11:32:06 +0800

    Add services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import ContactSection from '@/components/ContactSection';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
+import Services from '@/components/Services';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
       <ContactSection />
       <Footer />
     </main>
