b5fa811a4afaebf6f24eb37e8427a5749156b3b9
Aldrin <aljeromesulit@gmail.com>
2025-03-05 19:02:25 +0800

    Add Feature Projects section initial UI and the Flipped wave background

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,33 +1,41 @@
 import React from 'react'
 import Image from 'next/image'
+import FlippedWave from '@/components/svg/FlippedWave';
 
 type TProject = {
   img: string;
-}
+};
 
 const PROJECT_LIST: TProject[] = [
   {
-    img: "/project1.png"
+    img: '/project1.png',
   },
   {
-    img: "/project2.jpg"
+    img: '/project2.jpg',
   },
   {
-    img: "/project3.jpg"
-  }
-]
+    img: '/project3.jpg',
+  },
+];
 
 export default function Gallery() {
   return (
-    <section id='gallery' className='py-20'>
-      <h2 className='text-center font-notoSerifDisplay text-7xl'>Featured Projects</h2>
-      <ul className='flex gap-8 justify-center flex-wrap mt-16 font-semibold'>
-        {PROJECT_LIST.map((project, i)=>(
+    <section id='gallery' className='relative py-20'>
+      <FlippedWave className='absolute top-0 z-0 text-secondary' />
+      <h2 className='relative z-10 mt-40 text-center font-notoSerifDisplay text-7xl'>Featured Projects</h2>
+      <ul className='mt-16 flex flex-wrap justify-center gap-8 font-semibold'>
+        {PROJECT_LIST.map((project, i) => (
           <div key={i}>
-            <Image src={project.img} alt={`My project ${i + 1}`} width={800} height={450} className='w-96 h-72 object-cover rounded-sm' />
+            <Image
+              src={project.img}
+              alt={`My project ${i + 1}`}
+              width={800}
+              height={450}
+              className='h-72 w-96 rounded-sm object-cover'
+            />
           </div>
         ))}
       </ul>
     </section>
-  )
+  );
 }
---
+++ src/components/svg/FlippedWave.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FlippedWave(props: { className?: string }) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320' fill='currentColor' {...props}>
+      <path
+        fillOpacity='1'
+        d='M0,224L48,202.7C96,181,192,139,288,117.3C384,96,480,96,576,117.3C672,139,768,181,864,202.7C960,224,1056,224,1152,186.7C1248,149,1344,75,1392,37.3L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
+      ></path>
+    </svg>
+  );
+}
