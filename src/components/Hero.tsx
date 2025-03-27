e201057ecb7c784effbfd5436aa28a32908d0083
Aldrin <aljeromesulit@gmail.com>
2025-03-27 21:46:42 +0800

    Add marquee link and backgroun overlay image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,45 +1,81 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Marquee from "react-fast-marquee";
-
-import { cn } from '@/lib/utils';
+import Marquee from 'react-fast-marquee';
 
 export default function Hero() {
   return (
     <section id='home' className='relative h-dvh'>
       <div className='flex h-full justify-center pt-24'>
-        <div className='pt-20'>
-            <Image
-              src='/hero_profile.png'
-              alt='My profile photo'
-              width={463}
-              height={648}
-              className='relative z-10 h-full w-full rounded-sm object-cover'
-            />
+        <div className='pb-[3.7rem] pt-20'>
+          <Image
+            src='/hero_profile.png'
+            alt='My profile photo'
+            width={463}
+            height={648}
+            className='relative z-10 h-full w-full rounded-sm object-cover'
+          />
         </div>
 
         <header className='flex w-[762px] flex-col justify-center'>
-          <h2 className='font-playfairDisplay mb-2 text-3xl'>Hi, I&apos;m Aljoy!</h2>
-          <h1 className='font-roboto mb-9 text-balance text-6xl'>
+          <h2 className='mb-2 font-playfairDisplay text-3xl'>Hi, I&apos;m Aljoy!</h2>
+          <h1 className='mb-9 text-balance font-roboto text-6xl'>
             Your Brand&apos;s Growth Partner in Digital Marketing & Creative Design
           </h1>
-          <p className='font-workSans mb-11 pr-20 text-xl text-muted-foreground'>
+          <p className='mb-11 pr-20 font-workSans text-xl text-muted-foreground'>
             Elevating your brand with strategic marketing and stunning visuals.
           </p>
           <Link
             href='/#services'
-            className='font-workSans block w-max rounded-xl bg-primary px-8 py-2 text-2xl text-primary-foreground'
+            className='block w-max rounded-xl bg-primary px-8 py-2 font-workSans text-2xl text-primary-foreground'
           >
             Learn More
           </Link>
         </header>
       </div>
-      <div className='absolute bottom-0'>
-        <Marquee>
-          I can be a React component, multiple React components, or just some text.
-        </Marquee>
-      </div>
+      <OverlayBackgroundImg />
+      <MarqueeSlidingLink />
     </section>
+  );
+}
+
+function MarqueeSlidingLink() {
+  const SERVICES = [
+    { linkText: 'Content Creation', href: '#' },
+    { linkText: 'Graphic Design', href: '#' },
+    { linkText: 'Video Editing', href: '#' },
+    { linkText: 'Social Media Management', href: '#' },
+  ];
+
+  return (
+    <div className='absolute bottom-0 left-0 right-0 flex h-[3.7rem] items-center bg-foreground text-2xl text-secondary-foreground'>
+      <Marquee autoFill={true} className='overflow-y-hidden'>
+        {SERVICES.map((service, i) => (
+          <span key={i} className='flex items-center'>
+            <Link
+              className='uppercase transition-colors duration-300 ease-in-out hover:text-[#9696f6]'
+              href={service.href}
+            >
+              {service.linkText}
+            </Link>
+            <span className='mx-8 text-5xl'>•</span>
+          </span>
+        ))}
+      </Marquee>
+    </div>
+  );
+}
+
+function OverlayBackgroundImg() {
+  return (
+    <div className='absolute inset-y-0 left-0 flex items-end pb-[3.7rem]'>
+      <Image
+        src='/hero_background_profile.png'
+        alt='My sideview photo'
+        width={471}
+        height={727}
+        className='object-cover'
+      />
+    </div>
   );
 }
