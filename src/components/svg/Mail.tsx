1f57356b05aad397ecbda09adb0e0a7e863b83b2
Aldrin <aljeromesulit@gmail.com>
2025-04-04 23:14:08 +0800

    fix: Change footer social icons to match actual figma design

--- src/components/svg/Mail.tsx
+++ src/components/svg/Mail.tsx
@@ -1,6 +1,6 @@
 export default function Mail(props: { className?: string }) {
   return (
-    <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <svg viewBox='0 -1 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
       <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
       <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
       <g id='SVGRepo_iconCarrier'>
