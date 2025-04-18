bd2b041aa72c01a2f5cf43015e5390ac61aad3d7
Aldrin <aljeromesulit@gmail.com>
2025-04-18 21:04:47 +0800

    Add Services section responsive for all screen size

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,7 +8,7 @@ export default function Services() {
   return (
     <section
       id='services'
-      className='relative px-5 pb-[90px] pt-[88px] text-primary-foreground xl:pb-[178px] xl:pt-[176px]'
+      className='relative px-5 pb-[90px] pt-[88px] text-primary-foreground md:px-10 md:pt-[214px] xl:pb-[178px] xl:pt-[176px]'
     >
       <Vortex
         backgroundColor='black'
@@ -17,26 +17,36 @@ export default function Services() {
         baseHue={198}
         rangeSpeed={0.5}
         rangeRadius={1.2}
+        className='pointer-events-none'
       >
-        <div className='absolute inset-x-0 top-0 z-10 h-[390px] bg-[url(/purple_gradient_services.png)] bg-cover bg-center' />
+        <div className='absolute inset-x-0 top-0 z-10 h-[390px] bg-[url(/purple_gradient_services.png)] bg-cover bg-center lg:hidden' />
         <div className='absolute inset-x-0 bottom-0 z-10 h-[390px] bg-[url(/purple_yellow.png)] bg-cover bg-center opacity-75' />
       </Vortex>
       <header className='relative z-20'>
         <h3
-          className={`${roboto.className} mx-auto text-center text-xl/none font-medium lg:w-[1055px] lg:text-left xl:text-4xl`}
+          className={`${roboto.className} mx-auto text-center text-xl/none font-medium md:text-4xl lg:text-center lg:text-3xl xl:px-[140px] xl:text-4xl 2xl:px-[120px] 2xl:text-5xl min-[1900px]:text-6xl`}
         >
           I help businesses cut through the noise with strategic marketing, engaging content, and proven growth
           strategies.
         </h3>
       </header>
-      <article className='mt-[50px] flex flex-col justify-center gap-8 lg:flex-row'>
-        <div className='flex flex-col justify-between gap-4 rounded-3xl border border-solid border-[rgba(255,255,255,0.2)] bg-[rgba(255,255,255,0.22)] backdrop-blur-[1.5px] xl:w-[415px]'>
-          <div className='space-y-3 px-[22px] pt-[27px]'>
-            <h4 className={`${outfit.className} text-xs uppercase text-[#bfbbbb]`}>Social Media Management</h4>
-            <p className={`${roboto.className} text-xs/tight xl:text-[22px] xl:leading-6`}>
+      <article className='mt-[50px] flex flex-col justify-center gap-8 md:mt-[70px] md:gap-12 md:px-10 lg:flex-row lg:gap-8 lg:px-0 min-[1900px]:gap-14'>
+        <div className='flex flex-col justify-between gap-4 rounded-3xl border border-solid border-[rgba(255,255,255,0.2)] bg-[rgba(255,255,255,0.22)] backdrop-blur-[1.5px] md:border-2 lg:w-[415px] lg:flex-initial min-[1900px]:w-[550px]'>
+          <div className='space-y-3 px-[22px] pt-[27px] md:space-y-5'>
+            <h4
+              className={`${outfit.className} text-xs uppercase text-[#bfbbbb] md:text-3xl lg:text-xl xl:text-base min-[1900px]:text-2xl`}
+            >
+              Social Media Management
+            </h4>
+            <p
+              className={`${roboto.className} text-xs/tight md:text-xl lg:text-2xl xl:text-[1.375rem] xl:leading-6 min-[1900px]:text-3xl`}
+            >
               I turn social media into a powerful tool for brand growth with compelling content and smart strategies.
             </p>
-            <Link href='/works' className={`${robotoFlex.className} block text-sm xl:text-lg`}>
+            <Link
+              href='/works'
+              className={`${robotoFlex.className} block text-sm text-primary md:text-2xl lg:text-xl min-[1900px]:text-3xl`}
+            >
               How I help
               <span className='ml-2'>
                 <Arrow className='inline w-5' />
@@ -54,10 +64,16 @@ export default function Services() {
           </div>
         </div>
 
-        <div className='flex flex-col justify-between gap-4 rounded-3xl border border-solid border-[rgba(255,255,255,0.2)] bg-[rgba(255,255,255,0.25)] backdrop-blur-[1.5px] xl:w-[415px]'>
-          <div className='space-y-3 px-[22px] pt-[27px]'>
-            <h4 className={`${outfit.className} text-xs uppercase text-[#bfbbbb]`}>Graphic Design/Video Edit</h4>
-            <p className={`${roboto.className} text-xs/tight xl:text-[22px] xl:leading-6`}>
+        <div className='flex flex-col justify-between gap-4 rounded-3xl border border-solid border-[rgba(255,255,255,0.2)] bg-[rgba(255,255,255,0.25)] backdrop-blur-[1.5px] md:border-2 lg:w-[415px] lg:flex-initial min-[1900px]:w-[570px]'>
+          <div className='space-y-3 px-[22px] pt-[27px] md:space-y-5'>
+            <h4
+              className={`${outfit.className} text-xs uppercase text-[#bfbbbb] md:text-3xl lg:text-xl xl:text-base min-[1900px]:text-2xl`}
+            >
+              Graphic Design/Video Edit
+            </h4>
+            <p
+              className={`${roboto.className} text-xs/tight md:text-xl lg:text-2xl xl:text-[1.375rem] xl:leading-6 min-[1900px]:text-3xl`}
+            >
               I create visually compelling designs and high-quality video edits that bring ideas to life.
             </p>
             <a
@@ -65,7 +81,7 @@ export default function Services() {
               target='_blank'
               rel='noopener noreferrer'
               aria-label='Visit my works'
-              className={`${robotoFlex.className} block text-sm xl:text-lg`}
+              className={`${robotoFlex.className} block text-sm text-primary md:text-2xl lg:text-xl min-[1900px]:text-3xl`}
             >
               See my work
               <span className='ml-2'>
