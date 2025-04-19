a5448f527360c0d1e51bbb1c7ed6c38fe7479a37
Aldrin <aljeromesulit@gmail.com>
2025-04-19 23:57:13 +0800

    Initial commit for the react email template UI

--- src/emails/email-template.tsx
+++ src/emails/email-template.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Html, Head, Tailwind, Body, Container } from '@react-email/components';
+import EmailTemplateIcon from '@/components/svg/EmailTemplateIcon';
 
 interface EmailTemplateProps {
   fullName: string;
@@ -7,10 +9,20 @@ interface EmailTemplateProps {
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ fullName, company, message }) => (
-  <div>
-    <h1>
-      Welcome, {fullName}! From {company}
-    </h1>
-    <p>{message}</p>
-  </div>
+  <Html lang='en'>
+    <Head />
+    <Body>
+      <Tailwind>
+        <Container className='bg-gray-400'>
+          <EmailTemplateIcon />
+          <h1>
+            Welcome, {fullName}! From {company}
+          </h1>
+          <p>{message}</p>
+        </Container>
+      </Tailwind>
+    </Body>
+  </Html>
 );
+
+export default EmailTemplate;
