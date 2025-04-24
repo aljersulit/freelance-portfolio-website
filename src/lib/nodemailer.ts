f731d61128a71b0999c27ba72b1b7bb4af953c51
Aldrin <aljeromesulit@gmail.com>
2025-04-24 12:39:59 +0800

    fix: Fix email sending not working because of the initialization of nodemailer transporter

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,39 +1,15 @@
 import nodemailer from 'nodemailer';
 
-function getTransportObject() {
-  const environment = process.env.NODE_ENV;
-  let transportObject = null;
-
-  switch (environment) {
-    case 'development':
-      transportObject = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-          user: 'jeremie.pagac39@ethereal.email',
-          pass: 'TWnQ6TXuvY4fM2CaRu',
-        },
-      };
-      break;
-    case 'production':
-      transportObject = {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
-        auth: {
-          user: process.env.GMAIL_USER,
-          pass: process.env.GMAIL_PASSWORD,
-        },
-      };
-    default:
-      throw new Error('NODE_ENV environment variable not properly set.');
-  }
-
-  return transportObject;
-}
-
 export async function sendEmail(mailOptions: nodemailer.SendMailOptions) {
   try {
-    const transporter = nodemailer.createTransport(getTransportObject());
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
 
     const result = await transporter.sendMail(mailOptions);
     return result;
@@ -41,3 +17,12 @@ export async function sendEmail(mailOptions: nodemailer.SendMailOptions) {
     throw error;
   }
 }
+
+// const testTransportObject = {
+//   host: 'smtp.ethereal.email',
+//   port: 587,
+//   auth: {
+//     user: 'jeremie.pagac39@ethereal.email',
+//     pass: 'TWnQ6TXuvY4fM2CaRu',
+//   },
+// };
