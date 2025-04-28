73a53121abcb05bf7562e81661cf88d60ad3f6ab
Aldrin <aljeromesulit@gmail.com>
2025-04-28 13:32:25 +0800

    Adjust hero text font sizing for 1536pixel width screen breakpoint

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,34 +7,33 @@ import HeroProfile from '@/assets/hero_profile.png';
 export default function Hero() {
   return (
     <section id='home' className='relative flex h-lvh'>
-      <div className='@container relative flex w-full flex-col-reverse items-center justify-between gap-3 px-[21px] pt-[70px] md:px-[28px] md:pt-[90px] lg:mt-[75px] lg:flex-row lg:items-stretch lg:justify-center lg:gap-0 lg:px-0 lg:pt-0 xl:mt-[96px] 2xl:mt-[125px]'>
+      <div className='@container relative flex w-full flex-col-reverse items-center justify-between gap-3 px-[21px] pt-[70px] sm:pt-[90px] md:px-[28px] lg:mt-[75px] lg:flex-row lg:items-stretch lg:justify-center lg:gap-0 lg:px-0 lg:pt-0 xl:mt-[96px] 2xl:mt-[125px]'>
         <div className='max-h-max min-h-[50px] flex-1 pb-[24px] md:max-h-[750px] lg:h-full lg:max-h-none lg:w-auto lg:flex-initial lg:pb-[45px] lg:pt-20 xl:pb-[60px]'>
           <Image
             src={HeroProfile}
             alt='My profile photo'
-            placeholder='blur'
             priority
             className='relative z-10 h-full w-full rounded-sm object-contain md:-translate-x-3 md:object-cover lg:translate-x-0'
           />
         </div>
 
-        <header className='flex flex-col justify-center lg:w-[500px] xl:w-[762px] 2xl:w-[1200px]'>
+        <header className='flex flex-col justify-center lg:w-[500px] xl:w-[762px] 3xl:w-[1200px]'>
           <h2
-            className={`${playfairDisplay.className} playfairDisplay mb-2 hidden text-3xl lg:block 2xl:mb-6 2xl:text-6xl`}
+            className={`${playfairDisplay.className} playfairDisplay mb-2 hidden text-3xl lg:block 2xl:mb-6 2xl:text-4xl 3xl:text-6xl`}
           >
             Hi, I&apos;m Aljoy!
           </h2>
           <h1
-            className={`${roboto.className} mb-4 text-[1.7rem] font-bold leading-[1.1] min-[375px]:text-3xl min-[425px]:text-4xl md:mb-6 md:text-5xl lg:mb-8 lg:text-balance lg:text-6xl lg:font-semibold 2xl:mb-9 2xl:text-8xl`}
+            className={`${roboto.className} mb-4 text-[1.7rem] font-bold leading-[1.1] min-[375px]:text-3xl min-[425px]:text-4xl md:mb-6 md:text-5xl lg:mb-8 lg:text-balance lg:text-6xl lg:font-semibold 2xl:mb-9 3xl:text-8xl`}
           >
             Your Brand&apos;s Growth Partner in Digital Marketing & Creative Design
           </h1>
-          <p className='mb-4 text-[0.625rem] text-muted-foreground min-[375px]:text-sm md:mb-6 md:text-xl lg:mb-11 lg:text-xl 2xl:mb-8 2xl:text-4xl'>
+          <p className='mb-4 text-[0.625rem] text-muted-foreground min-[375px]:text-sm md:mb-6 md:text-xl lg:mb-11 lg:text-xl 2xl:mb-8 3xl:text-4xl'>
             Elevating your brand with strategic marketing and stunning visuals.
           </p>
           <Link
             href='/#services'
-            className='block w-max cursor-dot rounded-sm bg-primary px-7 py-[0.4rem] text-xs font-medium text-primary-foreground transition-colors hover:bg-accent min-[375px]:text-sm md:rounded-lg md:text-lg lg:rounded-xl lg:px-8 lg:py-2 lg:text-2xl lg:font-normal 2xl:rounded-2xl 2xl:px-16 2xl:py-4 2xl:text-4xl'
+            className='block w-max cursor-dot rounded-sm bg-primary px-7 py-[0.4rem] text-xs font-medium text-primary-foreground transition-colors hover:bg-accent min-[375px]:text-sm md:rounded-md md:text-lg lg:rounded-lg lg:px-8 lg:py-2 lg:text-2xl lg:font-normal 3xl:rounded-2xl 3xl:px-16 3xl:py-4 3xl:text-4xl'
           >
             Learn More
           </Link>
