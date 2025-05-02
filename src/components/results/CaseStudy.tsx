f031db448e09a5da2a6c4689607fe956aa87c447
Aldrin <aljeromesulit@gmail.com>
2025-05-02 22:05:11 +0800

    Add results page CaseStudy Section responsiveness

--- src/components/results/CaseStudy.tsx
+++ src/components/results/CaseStudy.tsx
@@ -7,65 +7,75 @@ import ListCheckmark from '@/components/svg/ListCheckmark';
 
 export default function CaseStudy() {
   return (
-    <section className={`${roboto.className} justify-center lg:flex xl:gap-[96px] xl:py-[120px]`}>
-      <div className='xl:w-[668px]'>
+    <section
+      className={`${roboto.className} justify-center px-[18px] pb-[30px] pt-[10px] min-[375px]:px-[22px] min-[375px]:pt-[20px] min-[425px]:px-[32px] sm:py-[45px] md:px-[42px] lg:flex lg:gap-[50px] lg:px-[20px] xl:gap-[96px] xl:pb-[120px] 2xl:pb-[140px] 3xl:pt-[140px]`}
+    >
+      <div className='lg:w-[470px] xl:w-[668px]'>
         <Image
           src={CaseStudyImg}
           alt='A collection of social media advertisements for Agribank.'
           className='h-auto xl:w-full'
         />
-        <p className='mt-[8px] text-sm'>
+        <p className='mt-[8px] text-pretty min-[425px]:text-base sm:text-xl lg:mt-[12px] lg:text-sm'>
           Agribank is a financial institution offering a wide range of banking and financial services. As the Social
           Media Manager & Graphic Artist, I managed Agribank&apos;s social media presence across multiple platforms,
           combining strategic content creation, graphic design, and video production to enhance engagement and promote
           financial services.
         </p>
       </div>
-      <div className='text-sm xl:w-[474px]'>
+      <div className='mt-[26px] text-xs min-[425px]:text-sm sm:mt-[36px] sm:text-lg lg:mt-0 lg:w-[390px] lg:text-xs xl:w-[474px] xl:text-sm'>
         <div>
-          <h2 className={`${robotoCondensed.className} uppercase xl:text-2xl`}>Goals</h2>
-          <ul className='space-y-3 xl:mt-[22px]'>
-            <li className='xl:pl-[48px] relative '>
-              <ListArrowRight className='absolute left-0 w-[32px] text-primary' />
+          <h2
+            className={`${robotoCondensed.className} text-xl uppercase min-[425px]:text-2xl sm:text-4xl lg:text-2xl xl:text-3xl`}
+          >
+            Goals
+          </h2>
+          <ul className='mt-[10px] space-y-3 sm:mt-[20px] md:mt-[16px] xl:mt-[22px]'>
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListArrowRight className='absolute left-0 w-[20px] text-primary sm:w-[26px] xl:w-[32px]' />
               <span>Develop tailored content for multiple platforms while maintaining a cohesive brand identity.</span>
             </li>
-            <li className='xl:pl-[48px] relative '>
-              <ListArrowRight className='absolute left-0 w-[32px] text-primary' />
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListArrowRight className='absolute left-0 w-[20px] text-primary sm:w-[26px] xl:w-[32px]' />
               <span>Drive audience engagement and increase inquiries through strategic content creation.</span>
             </li>
-            <li className='xl:pl-[48px] relative '>
-              <ListArrowRight className='absolute left-0 w-[32px] text-primary' />
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListArrowRight className='absolute left-0 w-[20px] text-primary sm:w-[26px] xl:w-[32px]' />
               <span>
                 Strengthen brand awareness by showcasing financial spanroducts, services, and company milestones.
               </span>
             </li>
           </ul>
         </div>
-        <div className='xl:mt-[40px]'>
-          <h2 className={`${robotoCondensed.className} uppercase xl:text-2xl`}>Strategy</h2>
-          <ul className='space-y-3 xl:mt-[22px]'>
-            <li className='xl:pl-[48px] relative'>
-              <ListCheckmark className='absolute left-0 w-[32px]' />
+        <div className='mt-[20px] xl:mt-[40px]'>
+          <h2
+            className={`${robotoCondensed.className} text-xl uppercase min-[425px]:text-2xl sm:text-4xl lg:text-2xl xl:text-3xl`}
+          >
+            Strategy
+          </h2>
+          <ul className='mt-[10px] space-y-3 sm:mt-[20px] md:mt-[16px] xl:mt-[22px]'>
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListCheckmark className='absolute left-0 w-[20px] sm:w-[26px] xl:w-[32px]' />
               <span>
                 Developed and maintained a structured content calendar, delivering three pieces of content per week,
                 including infographics, promotional videos, and educational posts.
               </span>
             </li>
-            <li className='xl:pl-[48px] relative'>
-            <ListCheckmark className='absolute left-0 w-[32px]' />
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListCheckmark className='absolute left-0 w-[20px] sm:w-[26px] xl:w-[32px]' />
               <span>
                 Balanced promotional content with engaging visuals, customer testimonials, and informative content
                 featuring clear calls to action (CTAs).
               </span>
             </li>
-            <li className='xl:pl-[48px] relative'>
-            <ListCheckmark className='absolute left-0 w-[32px]' />
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListCheckmark className='absolute left-0 w-[20px] sm:w-[26px] xl:w-[32px]' />
               <span>
                 Collaborated closely with my supervisor to craft impactful captions that matched the tone of each post.
               </span>
             </li>
-            <li className='xl:pl-[48px] relative'>
-            <ListCheckmark className='absolute left-0 w-[32px]' />
+            <li className='relative pl-[28px] min-[425px]:pl-[34px] sm:pl-[44px] xl:pl-[48px]'>
+              <ListCheckmark className='absolute left-0 w-[20px] sm:w-[26px] xl:w-[32px]' />
               <span>
                 Worked with internal teams to align content with ongoing promotions, internal events, and product
                 updates.
--- src/components/svg/ListArrowRight.tsx
+++ src/components/svg/ListArrowRight.tsx
@@ -2,7 +2,7 @@ import React, { SVGProps } from 'react';
 
 export default function ListArrowRight(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <svg viewBox='0 0 20.5 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
       <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
       <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round'></g>
       <g id='SVGRepo_iconCarrier'>
