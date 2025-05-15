82cd880c55de4dc797a2c8f7e7071fadbb382f99
Aldrin <aljeromesulit@gmail.com>
2025-05-15 17:31:55 +0800

    Add results page mobile devices full responsive

--- src/components/results/Takeaways.tsx
+++ src/components/results/Takeaways.tsx
@@ -4,10 +4,14 @@ import { outfit, playfairDisplay, roboto } from '@/app/font';
 export default function Takeaways() {
   return (
     <section className='pb-[70px] pt-[50px] lg:pb-[74px] lg:pt-[100px] xl:pb-[82px] xl:pt-[136px] 3xl:pb-[112px]'>
-      <div className='mx-auto w-11/12 rounded-lg border-[2px] border-primary bg-accent-foreground px-[16px] py-[8px] lg:max-w-[850px] lg:rounded-2xl lg:border-[4px] lg:px-[44px] lg:py-[24px] xl:max-w-[1066px] xl:rounded-3xl xl:border-[6px] xl:px-[64px] xl:py-[44px] 3xl:max-w-[1350px]'>
-        <h2 className={`${playfairDisplay.className} text-xl lg:text-4xl xl:text-5xl 3xl:text-6xl`}>Key Takeaways</h2>
+      <div className='mx-auto w-11/12 rounded-lg border-[2px] border-primary bg-accent-foreground px-[16px] py-[8px] sm:w-10/12 sm:rounded-xl sm:px-[24px] sm:py-[12px] lg:max-w-[850px] lg:rounded-2xl lg:border-[4px] lg:px-[44px] lg:py-[24px] xl:max-w-[1066px] xl:rounded-3xl xl:border-[6px] xl:px-[64px] xl:py-[44px] 3xl:max-w-[1350px]'>
+        <h2
+          className={`${playfairDisplay.className} text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 3xl:text-6xl`}
+        >
+          Key Takeaways
+        </h2>
         <ul
-          className={`${outfit.className} mt-[8px] list-disc pl-[16px] text-xs font-light lg:mt-[22px] lg:pl-[22px] lg:text-xl xl:mt-[42px] xl:pl-[30px] xl:text-2xl 3xl:text-3xl`}
+          className={`${outfit.className} mt-[8px] list-disc pl-[16px] text-xs font-light sm:pl-[18px] sm:text-base md:pl-[20px] md:text-lg lg:mt-[22px] lg:pl-[22px] lg:text-xl xl:mt-[42px] xl:pl-[30px] xl:text-2xl 3xl:text-3xl`}
         >
           <li>Proven ability to manage multi-platform content strategies.</li>
           <li>Strong skills in graphic design, video editing, and paid campaign management.</li>
@@ -16,22 +20,22 @@ export default function Takeaways() {
       </div>
 
       <h2
-        className={`${outfit.className} mt-[54px] text-center text-4xl font-medium lg:mt-[110px] lg:text-4xl xl:mt-[140px] xl:text-5xl 3xl:text-6xl`}
+        className={`${outfit.className} mt-[54px] text-center text-4xl font-medium md:text-5xl lg:mt-[110px] lg:text-4xl xl:mt-[140px] xl:text-5xl 3xl:text-6xl`}
       >
         Got a project?
       </h2>
       <p
-        className={`${roboto.className} mt-[2px] text-center text-xs lg:mt-[2px] lg:text-xs xl:mt-[4px] xl:text-sm 3xl:text-base`}
+        className={`${roboto.className} mt-[2px] text-center text-xs md:mt-[2px] md:text-sm lg:text-xs xl:mt-[4px] xl:text-sm 3xl:text-base`}
       >
         Discover how we can work together
       </p>
 
       <div
-        className={`${outfit.className} mt-[34px] flex justify-center gap-[6px] text-center text-sm lg:mt-[38px] lg:gap-[8px] lg:text-xl xl:mt-[54px] xl:gap-[12px] xl:text-2xl 3xl:text-3xl`}
+        className={`${outfit.className} mt-[34px] flex justify-center gap-[6px] text-center text-sm md:gap-[8px] md:text-xl lg:mt-[38px] xl:mt-[54px] xl:gap-[12px] xl:text-2xl 3xl:text-3xl`}
       >
         <Link
           href='/#contact'
-          className='rounded-full bg-primary px-[12px] py-[5px] uppercase text-primary-foreground lg:w-[260px] lg:px-0 lg:py-[6px] xl:w-[308px] xl:py-[10px] 3xl:w-[400px] 3xl:py-[18px]'
+          className='rounded-full bg-primary px-[12px] py-[5px] uppercase text-primary-foreground md:w-[260px] md:px-0 md:py-[8px] xl:w-[308px] xl:py-[10px] 3xl:w-[400px] 3xl:py-[18px]'
         >
           Get In Touch
         </Link>
@@ -39,7 +43,7 @@ export default function Takeaways() {
           href='https://www.behance.net/alyssajoysulit'
           target='_blank'
           rel='noopener noreferrer'
-          className='rounded-full border border-foreground px-[10px] py-[4px] uppercase lg:w-[260px] lg:border-2 lg:px-0 lg:py-[6px] xl:w-[308px] xl:py-[10px] 3xl:w-[400px] 3xl:py-[18px]'
+          className='rounded-full border border-foreground px-[10px] py-[4px] uppercase md:w-[260px] md:border-2 md:px-0 md:py-[6px] xl:w-[308px] xl:py-[10px] 3xl:w-[400px] 3xl:py-[18px]'
         >
           Creative Portfolio
         </a>
