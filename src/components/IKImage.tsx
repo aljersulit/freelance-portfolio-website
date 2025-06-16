2aa8c2689ce542a4762eb50369a349472e11847f
Aldrin <aljeromesulit@gmail.com>
2025-06-16 20:53:02 +0800

    Fix lint errors

--- src/components/IKImage.tsx
+++ src/components/IKImage.tsx
@@ -1,11 +1,9 @@
 import { Image, ImageKitProvider, IKImageProps } from '@imagekit/next';
 
-export default function IKImage(props: IKImageProps) {
+export default function IKImage({ alt, ...rest }: IKImageProps) {
   return (
     <ImageKitProvider urlEndpoint='https://ik.imagekit.io/aljersulit'>
-      <Image
-        {...props}
-      />
+      <Image alt={alt} {...rest} />
     </ImageKitProvider>
-  )
+  );
 }
