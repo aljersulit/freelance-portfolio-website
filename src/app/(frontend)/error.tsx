ff606ae4420ec41d8728f410cbab2bf6ea4ec2fc
Aldrin <aljeromesulit@gmail.com>
2025-07-07 16:45:51 +0800

    fix: Fix the error component layout positioning

--- src/app/(frontend)/error.tsx
+++ src/app/(frontend)/error.tsx
@@ -9,7 +9,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   }, [error]);
 
   return (
-    <main className='flex h-full flex-col items-center justify-center'>
+    <main className='flex h-lvh flex-col items-center justify-center'>
       <h2 className='text-center'>Something went wrong!</h2>
       <button
         className='mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400'
